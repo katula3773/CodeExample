5218082a1c593198d3bd7cb08f6a7b53acfc227d
PhongnvPC <phongnv3773@gmail.com>
2017-01-11 01:29:08 +0700

    update demo

---
+++ ShowHideForm/ShowHideForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowHideForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCallForm_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            Form2 frm = new Form2();
+            frm.ShowDialog();
+
+            this.Show();
+        }
+    }
+}
---
+++ ShowHideForm/ShowHideForm/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowHideForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCallForm_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            Form3 frm = new Form3();
+            frm.ShowDialog();
+
+            this.Show();
+        }
+    }
+}
